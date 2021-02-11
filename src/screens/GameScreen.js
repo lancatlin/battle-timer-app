b55b2f3e50b688fa7040bb65ef9c16cb26350d5b
wancat <wancat@wancat.cc>
2021-02-11 12:39:51 +0800

    Read context

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
+import { Context } from '../context/GameContext'
 
 const GameScreen = () => {
+  const { state } = useContext(Context)
+  console.log(state)
   return (
     <SafeAreaView>
       <Text h1>Game</Text>
