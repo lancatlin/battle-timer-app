e9ec1ae4be577b3a4659d031ff1599408edad75f
wancat <wancat@wancat.cc>
2021-02-11 11:41:32 +0800

    HomeScreen input and styles

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Button, ThemeProvider } from 'react-native-elements'
+import { View, StyleSheet } from 'react-native'
+import { Button, ThemeProvider, Text, Input } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
-  return <SafeAreaView>
-    <Text>Home</Text>
+  return <SafeAreaView style={styles.container}>
+    <Text h1>Home</Text>
+    <Input label="Players" />
+    <Input label="Total Time" />
+    <Input label="Final Time" />
   </SafeAreaView>
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    marginHorizontal: 20,
+  }
+})
 
 export default HomeScreen
