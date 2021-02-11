335f9ecc2c5bb93d79ed182b515c830b95039887
wancat <wancat@wancat.cc>
2021-02-11 14:44:06 +0800

    little refactor

--- src/helper/randomColor.js
+++ src/helper/randomColor.js
@@ -1,6 +1,4 @@
 export default () => {
-  const color = Array.from([0, 0, 0], () => {
-    return Math.random() * 150 + 50
-  })
+  const color = Array.from([0, 0, 0], () => Math.random() * 150 + 50)
   return `rgb(${color.join(', ')})`
 }
